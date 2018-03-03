70919272f50ee943260cb42a61c714ffe3c89380
Ozimba SubZero <ozimbat@hotmail.com>
2018-03-03 11:09:13 -0400

    Add project files.

---
+++ StatementParse/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace StatementParse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_savings1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_savings1
+            // 
+            this.btn_savings1.Location = new System.Drawing.Point(13, 13);
+            this.btn_savings1.Name = "btn_savings1";
+            this.btn_savings1.Size = new System.Drawing.Size(114, 38);
+            this.btn_savings1.TabIndex = 0;
+            this.btn_savings1.Text = "Savings 1";
+            this.btn_savings1.UseVisualStyleBackColor = true;
+            this.btn_savings1.Click += new System.EventHandler(this.btn_savings1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn_savings1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_savings1;
+    }
+}
+
---
+++ StatementParse/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StatementParse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_savings1_Click(object sender, EventArgs e)
+        {
+            List<string> emortelleLines = new List<string>();
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Text Files (.txt)|*.txt";
+            openFile.Title = "Select the Emortelle Transaction Reports";
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader streamReader = new StreamReader(openFile.FileName))
+                    {
+                        string line = "";
+                        while ((line = streamReader.ReadLine()) != null)
+                        {
+                            emortelleLines.Add(line);
+                        }
+                    }
+                    GetTransactionLines(GetFixedWidth(emortelleLines), emortelleLines);
+                    emortelleLines.Clear();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read the file. Original error: " + ex.Message);
+                }
+            }
+        }
+
+
+
+        /* Extra Functions */
+
+        
+
+        private List<KeyValuePair<int, int>> GetFixedWidth(List<string> emortelleLines)
+        {
+            string fixedWidthLine = emortelleLines[7];
+            var fixedWidthListLines = fixedWidthLine.Split(' ').Select(g => g.Count()).ToList();
+            var fixedWidthList = new List<KeyValuePair<int, int>>();
+
+            int startIndex = 0;
+
+            for (int i = 0; i < fixedWidthListLines.Count(); i++)
+            {
+                var pair = new KeyValuePair<int, int>(startIndex, fixedWidthListLines[i]);
+                fixedWidthList.Add(pair);
+                startIndex += fixedWidthListLines[i] + 1;
+            }
+            return fixedWidthList;
+        }
+
+    }
+}
